b9a988dd07fbcd020a8e30f45993aad361da2e79
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-10-30 12:59:43 +0100

    Fix wrong folder name issue

--- CheckersTerminal/Program.cs
+++ CheckersTerminal/Program.cs
@@ -1,14 +1,34 @@
-﻿using CheckersLogic;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using CheckersTerminal.Draw;
+using CheckersTerminal.Update;
+using CheckersLogic.States;
 
 namespace CheckersTerminal
 {
     class Program
     {
+        static State currentState;
+        static State nextState;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Pawn t = new BlackDame();
+            Console.CursorVisible = false;
+            currentState = new MenuState();
+            while (true)
+            {
+                if (nextState != null)
+                {
+                    currentState = nextState;
+                    nextState = null;
+                }
+                currentState.Update();
+                currentState.Draw();
+            }
+        }
+
+        public static void ChangeState(State newState)
+        {
+            nextState = newState;
         }
     }
 }
---
+++ CheckersTerminal/Update/UpdateMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CheckersLogic.States;
+using CheckersTerminal;
+
+namespace CheckersTerminal.Update
+{
+    public static class UpdateMethods
+    {
+       public static void Update(this MenuState menuState)
+        {
+
+            ConsoleKeyInfo ckey = Console.ReadKey();
+
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (menuState.index == menuState.options.Count - 1)
+                {
+                    menuState.index = 0;
+                }
+                else { menuState.index++; }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (menuState.index <= 0)
+                {
+                    menuState.index = menuState.options.Count - 1;
+                }
+                else { menuState.index--; }
+            }
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                Program.ChangeState(menuState.options[menuState.index].OptionState);
+            }
+        }
+
+        public static void Update(this State State)
+        {
+            if(State is MenuState)
+            {
+                (State as MenuState).Update();
+            }
+            if (State is PlayerVsPlayer)
+            {
+                (State as PlayerVsPlayer).Update();
+            }
+        }
+
+        public static void Update(this PlayerVsPlayer pvp)
+        {
+            Console.WriteLine("PVP");
+        }
+    }
+}
